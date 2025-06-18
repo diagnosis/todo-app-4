29f0284304520fae2b0c93603da60b0959af5ed6
diagnosis <demirkansafa@gmail.com>
2025-06-18 12:35:01 -0700

    updates

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/rs/cors"
 	"log"
 	"net/http"
+	"os"
 	"todo-app-4/handlers"
 	"todo-app-4/repository"
 	"todo-app-4/routes"
@@ -13,7 +14,12 @@ import (
 )
 
 func main() {
-	connStr := "postgres://postgres:password@localhost:5431/todo_app?sslmode=disable"
+	connStr := os.Getenv("DATABASE_URL")
+	if connStr == "" {
+		// connStr = "postgres://postgres:password@localhost:5431/todo_app?sslmode=disable" // Local fallback
+		connStr = "postgresql://neondb_owner:npg_RVku9lz5Lvqm@ep-flat-star-a629jikr-pooler.us-west-2.aws.neon.tech/todo_app?sslmode=require" // Neon primary
+		log.Println("Warning: DATABASE_URL not set, using Neon connection")
+	}
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
@@ -24,6 +30,7 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Println("Connected to PostgreSQL!")
+
 	todoRepo := repository.NewTodoRepository(db)
 	todoService := services.NewTodoService(todoRepo)
 	todoHandler := handlers.NewTodoHandler(todoService)
@@ -34,5 +41,4 @@ func main() {
 	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
 	}
-
 }
