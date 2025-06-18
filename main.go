2c6f22b34e3ce80c1ce9dfd16f9a510ee0721807
diagnosis <demirkansafa@gmail.com>
2025-06-18 13:00:47 -0700

    updates

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/rs/cors"
 	"log"
@@ -14,10 +15,10 @@ import (
 )
 
 func main() {
+	godotenv.Load()
 	connStr := os.Getenv("DATABASE_URL")
 	if connStr == "" {
 		// connStr = "postgres://postgres:password@localhost:5431/todo_app?sslmode=disable" // Local fallback
-		connStr = "postgresql://neondb_owner:npg_RVku9lz5Lvqm@ep-flat-star-a629jikr-pooler.us-west-2.aws.neon.tech/todo_app?sslmode=require" // Neon primary
 		log.Println("Warning: DATABASE_URL not set, using Neon connection")
 	}
 	db, err := sql.Open("postgres", connStr)
